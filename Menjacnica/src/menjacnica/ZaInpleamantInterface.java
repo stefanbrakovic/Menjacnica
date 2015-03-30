ec9015c46b7140d40c36c694296d34a9b5a494f5
stefanbrakovic <brakovic.stefan@gmail.com>
2015-03-30 18:10:01 +0200

    Dodata klasa koja implementira interface ali jos nije menjana ni jedna metoda

---
+++ Menjacnica/src/menjacnica/ZaInpleamantInterface.java
@@ -0,0 +1,24 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import interfacePackage.Menjacnica;
+
+public class ZaInpleamantInterface implements Menjacnica {
+
+	public void newCourse(GregorianCalendar date) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void deleteCourse(GregorianCalendar date) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public double returnCourse(String name, GregorianCalendar date) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

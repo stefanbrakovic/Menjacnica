db5dc79b44bc8472f1621fa664543e43924cc65e
stefanbrakovic <brakovic.stefan@gmail.com>
2015-03-30 18:36:51 +0200

    Implementirana klasa za implementaciju intrfacea dodato u klasi Menjacnica da su valute staticke!

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -3,6 +3,13 @@ package menjacnica;
 import java.util.LinkedList;
 
 public class Menjacnica {
-	LinkedList<Valuta> valute = new LinkedList<Valuta>();
+	static LinkedList<Valuta> valute = new LinkedList<Valuta>();
+	
+//	static public LinkedList<Valuta> getValute()
+//	{
+//		return valute;
+//	}
+//	
+	
 
 }
--- Menjacnica/src/menjacnica/ZaInpleamantInterface.java
+++ Menjacnica/src/menjacnica/ZaInpleamantInterface.java
@@ -1,24 +1,43 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import interfacePackage.Menjacnica;
 
 public class ZaInpleamantInterface implements Menjacnica {
-
-	public void newCourse(GregorianCalendar date) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void deleteCourse(GregorianCalendar date) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
+	
 	public double returnCourse(String name, GregorianCalendar date) {
-		// TODO Auto-generated method stub
-		return 0;
+		LinkedList<Valuta> valute =  menjacnica.Menjacnica.valute;
+		for (int i = 0; i < valute.size(); i++) {
+			if (valute.get(i).getName().equals(name)&&valute.get(i).getDate().equals(date)) {
+				return valute.get(i).getMiddlePrice();
+				
+			}
+		}
+		return -1;
 	}
 
+	public void newCourse(String name, String shortName, GregorianCalendar date) {
+		LinkedList<Valuta> valute =  menjacnica.Menjacnica.valute;
+		Valuta valuta =  new Valuta(name, shortName, date, 0, 0, 0);
+		valute.add(valuta);
+		return;
+		
+	}
+
+	public void deleteCourse(String name, String shortName,
+			GregorianCalendar date) {
+		LinkedList<Valuta> valute =  menjacnica.Menjacnica.valute;
+		for (int i = 0; i < valute.size(); i++) {
+			if (valute.get(i).getName().equals(name)&&valute.get(i).getDate().equals(date)&&
+					valute.get(i).getShortName().equals(shortName)) {
+				valute.remove(i);
+				return;
+				}
+		
+	}
+
+	}
 }
